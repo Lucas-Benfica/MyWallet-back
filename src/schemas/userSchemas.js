a2f4fccb16aa5189ea073e7377609f20b8240426
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-07-11 15:48:40 -0300

    Configurações para deploy

--- src/schemas/userSchemas.js
+++ src/schemas/userSchemas.js
@@ -2,6 +2,6 @@ import Joi from "joi";
 
 export const schemaUser = Joi.object({
     name: Joi.string().required(),
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required().min(3)
 })
