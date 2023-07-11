6ae0a3d2c31ab0d1cdfe42f73bf3b79beed16a80
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-07-11 00:38:21 -0300

    add

--- src/app.js
+++ src/app.js
@@ -2,13 +2,15 @@
 import express from "express"
 import cors from "cors"
 import router from "./routes/indexRoutes.js"
+import dotenv from "dotenv";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(router);
-dotev.config()
+
+dotenv.config()
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
