7806533dc4c99415b967b9d79a1f149e85b7c333
Lucas-Benfica <lucassoaresbenfica@gmail.com>
2023-07-11 00:18:26 -0300

    chore: adjust code for deploy on render

--- src/app.js
+++ src/app.js
@@ -8,7 +8,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+dotev.config()
 
-const PORT = 5000
-app.listen(PORT, () => console.log(`Servidor está rodando na porta ${PORT}`));
-
+const port = process.env.PORT || 5000
+app.listen(port, () => {
+	console.log(`Servidor rodando na porta ${port}`)
+})
